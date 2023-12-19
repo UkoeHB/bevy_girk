69acc11d25d93119ffb4b989041f4fdb308d375e
koe <ukoe@protonmail.com>
2023-12-18 21:06:48 -0600

    add integration test for reconnecting a game client app while the app is still alive

--- tests/click_game_integration/integration_reconnects.rs
+++ tests/click_game_integration/integration_reconnects.rs
@@ -14,6 +14,7 @@ use bevy::prelude::*;
 use bevy_kot_ecs::*;
 use bevy_kot_utils::*;
 use bevy_renet::renet::RenetClient;
+use bevy_renet::renet::transport::ClientAuthentication;
 
 //standard shortcuts
 use std::net::Ipv6Addr;
@@ -201,7 +202,7 @@ fn launch_game(
     hub_server: &mut App,
     user1: &HostUserClient,
     user2: &HostUserClient,
-) -> (App, App)
+) -> (u64, App, App, GameStartInfo, GameStartInfo)
 {
     // wait for everything to start up
     std::thread::sleep(Duration::from_millis(15));
@@ -294,79 +295,28 @@ fn launch_game(
 
     // users 1, 2 make game clients
     // - we only make the cores here, no client skins
-    let (mut client_app1, _, _) = make_game_client_core(get_test_protocol_id(), connect1, start1);
-    let (mut client_app2, _, _) = make_game_client_core(get_test_protocol_id(), connect2, start2);
+    let (mut client_app1, _, _) = make_game_client_core(get_test_protocol_id(), connect1, start1.clone());
+    let (mut client_app2, _, _) = make_game_client_core(get_test_protocol_id(), connect2, start2.clone());
 
 
     // tick clients until the game is fully initialized
     tick_clients_until_game_initialized(vec![&mut client_app1, &mut client_app2]);
 
-    (client_app1, client_app2)
+    (game_id, client_app1, client_app2, start1, start2)
 }
 
 //-------------------------------------------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------------------------------------
 
-#[test]
-fn integration_reconnect_userclient_restart()
-{
-    // prepare tracing
-    /*
-    let subscriber = tracing_subscriber::FmtSubscriber::builder()
-        .with_max_level(tracing::Level::TRACE)
-        .finish();
-    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
-    */
-
-    // launch host server
-    let (mut host_server, host_hub_url, host_user_url) = make_test_host_server(make_host_server_test_configs());
-
-    // launch game hub server attached to host server
-    let game_ticks_per_sec = Ticks(10);
-    let game_num_ticks     = Ticks(15);
-    let (_hub_command_sender, mut hub_server) = make_test_game_hub_server(
-            host_hub_url,
-            make_hub_server_test_configs(),
-            make_click_game_test_configs(game_ticks_per_sec, game_num_ticks)
-        );
-
-    // make user clients
-    let user1_id = 0u128;
-    let user2_id = 1u128;
-    let (user1_id, user1) = make_test_host_user_client_with_id(user1_id, host_user_url.clone());
-    let (_, user2)        = make_test_host_user_client_with_id(user2_id, host_user_url.clone());
-
-
-    // launch game
-    let (_, mut client_app2) = launch_game(&mut host_server, &mut hub_server, &user1, &user2);
-
-
-    // disconnect client 1
-    // - we do this *before* the server times out the connection to make sure rapid reconnects work
-    user1.close();
-    host_server.update(); hub_server.update(); std::thread::sleep(Duration::from_millis(45));
-
-    // reconnect client 1
-    let (user1_id, user1) = make_test_host_user_client_with_id(user1_id, host_user_url.clone());
-    host_server.update(); hub_server.update(); std::thread::sleep(Duration::from_millis(45));
-    host_server.update(); hub_server.update(); std::thread::sleep(Duration::from_millis(45));
-
-    let HostUserClientEvent::Report(_) = user1.next().unwrap() else { unimplemented!(); };
-
-    // receive game start on reconnect
-    let Some(HostUserClientEvent::Msg(HostToUserMsg::GameStart{ id, connect: connect1, start: start1 })) = user1.next()
-    else { panic!("client did not receive server msg"); };
-    assert_eq!(user1_id, start1.user_id);
-    let game_id = id;
-
-    // remake game client 1
-    let (mut client_app1, _, _) = make_game_client_core(get_test_protocol_id(), connect1, start1);
-
-
-    // tick clients until the game is fully initialized for the reconnected client
-    tick_clients_until_game_initialized(vec![&mut client_app1, &mut client_app2]);
-
-
+fn game_end_cleanup(
+    mut host_server: App,
+    mut hub_server: App,
+    user1: HostUserClient,
+    user2: HostUserClient,
+    mut client_app1: App,
+    mut client_app2: App,
+    game_id: u64,
+){
     // wait for game over
     let mut report1: Option<GameOverReport> = None;
     let mut report2: Option<GameOverReport> = None;
@@ -403,6 +353,210 @@ fn integration_reconnect_userclient_restart()
     let _ = user1.close();
     let _ = user2.close();
     std::thread::sleep(Duration::from_millis(15));
+}
+
+//-------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------
+
+// Reconnect by replacing the game client.
+#[test]
+fn integration_reconnect_gameclient_restart()
+{
+    // prepare tracing
+    /*
+    let subscriber = tracing_subscriber::FmtSubscriber::builder()
+        .with_max_level(tracing::Level::TRACE)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+    */
+
+    // launch host server
+    let (mut host_server, host_hub_url, host_user_url) = make_test_host_server(make_host_server_test_configs());
+
+    // launch game hub server attached to host server
+    let game_ticks_per_sec = Ticks(10);
+    let game_num_ticks     = Ticks(15);
+    let (_hub_command_sender, mut hub_server) = make_test_game_hub_server(
+            host_hub_url,
+            make_hub_server_test_configs(),
+            make_click_game_test_configs(game_ticks_per_sec, game_num_ticks)
+        );
+
+    // make user clients
+    let user1_id = 0u128;
+    let user2_id = 1u128;
+    let (_, user1) = make_test_host_user_client_with_id(user1_id, host_user_url.clone());
+    let (_, user2)        = make_test_host_user_client_with_id(user2_id, host_user_url.clone());
+
+
+    // launch game
+    let (game_id, client_app1, mut client_app2, start1, _) = launch_game(&mut host_server, &mut hub_server, &user1, &user2);
+
+
+    // disconnect game client 1
+    std::mem::drop(client_app1);
+    host_server.update(); hub_server.update(); std::thread::sleep(Duration::from_millis(45));
+
+    // request new connect token for client 1
+    user1.request(UserToHostRequest::GetConnectToken{ id: game_id }).expect("send failed");
+    std::thread::sleep(Duration::from_millis(15));
+    host_server.update(); hub_server.update(); std::thread::sleep(Duration::from_millis(15));
+
+    // - receive new connect token
+    let Some(HostUserClientEvent::Response(HostToUserResponse::ConnectToken{ id, connect }, _)) = user1.next()
+    else { panic!("client did not receive server msg"); };
+    assert_eq!(id, game_id);
+
+
+    // remake game client 1
+    let (mut client_app1, _, _) = make_game_client_core(get_test_protocol_id(), connect, start1);
+
+
+    // tick clients until the game is fully initialized for the reconnected client
+    tick_clients_until_game_initialized(vec![&mut client_app1, &mut client_app2]);
+
+
+    // cleanup
+    game_end_cleanup(host_server, hub_server, user1, user2, client_app1, client_app2, game_id);
+}
+
+//-------------------------------------------------------------------------------------------------------------------
+
+// Reconnect while the game app is still alive.
+#[test]
+fn integration_reconnect_gameclient_reconnect()
+{
+    // prepare tracing
+    /*
+    let subscriber = tracing_subscriber::FmtSubscriber::builder()
+        .with_max_level(tracing::Level::TRACE)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+    */
+
+    // launch host server
+    let (mut host_server, host_hub_url, host_user_url) = make_test_host_server(make_host_server_test_configs());
+
+    // launch game hub server attached to host server
+    let game_ticks_per_sec = Ticks(10);
+    let game_num_ticks     = Ticks(15);
+    let (_hub_command_sender, mut hub_server) = make_test_game_hub_server(
+            host_hub_url,
+            make_hub_server_test_configs(),
+            make_click_game_test_configs(game_ticks_per_sec, game_num_ticks)
+        );
+
+    // make user clients
+    let user1_id = 0u128;
+    let user2_id = 1u128;
+    let (_, user1) = make_test_host_user_client_with_id(user1_id, host_user_url.clone());
+    let (_, user2) = make_test_host_user_client_with_id(user2_id, host_user_url.clone());
+
+
+    // launch game
+    let (game_id,mut client_app1,mut client_app2, _, _) = launch_game(&mut host_server, &mut hub_server, &user1, &user2);
+
+
+    // disconnect game client 1 from renet server
+    client_app1.world.resource_mut::<RenetClient>().disconnect();
+    client_app1.update();
+    host_server.update(); hub_server.update(); std::thread::sleep(Duration::from_millis(45));
+    client_app1.update(); std::thread::sleep(Duration::from_millis(45));
+    assert!(client_app1.world.resource::<RenetClient>().is_connecting());
+    assert_eq!(*client_app1.world.resource::<State<ClientInitializationState>>().get(), ClientInitializationState::InProgress);
+
+    // request new connect token for client 1
+    user1.request(UserToHostRequest::GetConnectToken{ id: game_id }).expect("send failed");
+    std::thread::sleep(Duration::from_millis(15));
+    host_server.update(); hub_server.update(); std::thread::sleep(Duration::from_millis(15));
+
+    // - receive new connect token
+    let Some(HostUserClientEvent::Response(HostToUserResponse::ConnectToken{ id, connect }, _)) = user1.next()
+    else { panic!("client did not receive server msg"); };
+    assert_eq!(id, game_id);
+
+
+    // reconnect game client app
+    let ServerConnectToken::Native{ bytes } = connect;
+    let connect_token = connect_token_from_bytes(&bytes).unwrap();
+    let client_address = client_address_from_server_address(&connect_token.server_addresses[0].unwrap());
+    let connect_pack = RenetClientConnectPack::Native(ClientAuthentication::Secure{ connect_token }, client_address);
+    client_app1.insert_resource(connect_pack);
+    setup_renet_client(&mut client_app1.world);
+
+
+    // tick clients until the game is fully initialized for the reconnected client
+    tick_clients_until_game_initialized(vec![&mut client_app1, &mut client_app2]);
+
+
+    // cleanup
+    game_end_cleanup(host_server, hub_server, user1, user2, client_app1, client_app2, game_id);
+}
+
+//-------------------------------------------------------------------------------------------------------------------
+
+#[test]
+fn integration_reconnect_userclient_restart()
+{
+    // prepare tracing
+    /*
+    let subscriber = tracing_subscriber::FmtSubscriber::builder()
+        .with_max_level(tracing::Level::TRACE)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+    */
+
+    // launch host server
+    let (mut host_server, host_hub_url, host_user_url) = make_test_host_server(make_host_server_test_configs());
+
+    // launch game hub server attached to host server
+    let game_ticks_per_sec = Ticks(10);
+    let game_num_ticks     = Ticks(15);
+    let (_hub_command_sender, mut hub_server) = make_test_game_hub_server(
+            host_hub_url,
+            make_hub_server_test_configs(),
+            make_click_game_test_configs(game_ticks_per_sec, game_num_ticks)
+        );
+
+    // make user clients
+    let user1_id = 0u128;
+    let user2_id = 1u128;
+    let (user1_id, user1) = make_test_host_user_client_with_id(user1_id, host_user_url.clone());
+    let (_, user2)        = make_test_host_user_client_with_id(user2_id, host_user_url.clone());
+
+
+    // launch game
+    let (game_id, _, mut client_app2, _, _) = launch_game(&mut host_server, &mut hub_server, &user1, &user2);
+
+
+    // disconnect client 1
+    // - we do this *before* the server times out the connection to make sure rapid reconnects work
+    user1.close();
+    host_server.update(); hub_server.update(); std::thread::sleep(Duration::from_millis(45));
+
+    // reconnect client 1
+    let (user1_id, user1) = make_test_host_user_client_with_id(user1_id, host_user_url.clone());
+    host_server.update(); hub_server.update(); std::thread::sleep(Duration::from_millis(45));
+    host_server.update(); hub_server.update(); std::thread::sleep(Duration::from_millis(45));
+
+    let HostUserClientEvent::Report(_) = user1.next().unwrap() else { unimplemented!(); };
+
+    // receive game start on reconnect
+    let Some(HostUserClientEvent::Msg(HostToUserMsg::GameStart{ id, connect: connect1, start: start1 })) = user1.next()
+    else { panic!("client did not receive server msg"); };
+    assert_eq!(user1_id, start1.user_id);
+    assert_eq!(id, game_id);
+
+    // remake game client 1
+    let (mut client_app1, _, _) = make_game_client_core(get_test_protocol_id(), connect1, start1);
+
+
+    // tick clients until the game is fully initialized for the reconnected client
+    tick_clients_until_game_initialized(vec![&mut client_app1, &mut client_app2]);
+
+
+    // cleanup
+    game_end_cleanup(host_server, hub_server, user1, user2, client_app1, client_app2, game_id);
 }
 
 //-------------------------------------------------------------------------------------------------------------------
