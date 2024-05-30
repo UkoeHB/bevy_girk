273fc56e7a023f489c21c38253d5f6ebd2916f8a
koe <ukoe@protonmail.com>
2024-05-29 19:00:49 -0500

    add ReplicateRepairReactExt

--- bevy_girk_utils/src/lib.rs
+++ bevy_girk_utils/src/lib.rs
@@ -6,6 +6,7 @@ mod misc_utils;
 mod network_setup;
 mod network_utils;
 mod rand64;
+mod replicate_repair_react_ext;
 mod serialization;
 mod tick_counter;
 
@@ -13,15 +14,16 @@ mod tick_counter;
 mod child_process_utils;
 
 //API exports
-pub use crate::channel::*;
-pub use crate::cli::*;
-pub use crate::io_channel::*;
-pub use crate::misc_utils::*;
-pub use crate::network_setup::*;
-pub use crate::network_utils::*;
-pub use crate::rand64::*;
-pub use crate::serialization::*;
-pub use crate::tick_counter::*;
+pub use channel::*;
+pub use cli::*;
+pub use io_channel::*;
+pub use misc_utils::*;
+pub use network_setup::*;
+pub use network_utils::*;
+pub use rand64::*;
+pub use replicate_repair_react_ext::*;
+pub use serialization::*;
+pub use tick_counter::*;
 
 #[cfg(not(target_family = "wasm"))]
-pub use crate::child_process_utils::*;
+pub use child_process_utils::*;
